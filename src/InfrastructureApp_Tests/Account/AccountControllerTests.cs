c7dd9be3b1d4cc41341ba9c5308d91f9d5442a84
Phillip Alter PC <phillip.alter@outlook.com>
2026-02-04 16:10:18 -0800

    (add) add setup & teardown methods; add WhenModelStateInvalid test

---
+++ src/InfrastructureApp_Tests/Account/AccountControllerTests.cs
@@ -0,0 +1,41 @@
+using InfrastructureApp.Controllers;
+using InfrastructureApp.Models;
+using InfrastructureApp.ViewModels.Account;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace InfrastructureApp_Tests;
+
+[TestFixture]
+public class AccountControllerTests
+{
+    private Mock<UserManager<Users>> _mockUserManager;
+    private AccountController _controller;
+
+    [SetUp]
+    public void Setup()
+    {
+        var store = new Mock<IUserStore<Users>>();
+        _mockUserManager = new Mock<UserManager<Users>>(store.Object, null, null, null, null, null, null, null, null);
+        _controller = new AccountController(_mockUserManager.Object);
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        _controller?.Dispose();
+    }
+
+    [Test]
+    public async Task RegisterPort_ReturnsView_WhenModelStateInvalid()
+    {
+        _controller.ModelState.AddModelError("Email", "Required");
+        var model = new RegisterViewModel();
+
+        var result = await _controller.Register(model);
+
+        var viewResult = result as ViewResult;
+        Assert.That(model, Is.EqualTo(viewResult.Model));
+    }
+}
