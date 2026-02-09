c2d75f3982ca1567ad5f0b9bbd4d192f5663274c
Julian Aguilar-Tomas <julianaguilar.tomas17@gmail.com>
2026-02-09 00:16:13 -0800

    Added LatestReportsViewModel to pass report data to the view.

---
+++ src/InfrastructureApp/ViewModels/LatestReportsViewModel.cs
@@ -0,0 +1,9 @@
+namespace InfrastructureApp.ViewModels
+{
+    public class LatestReportsViewModel
+    {
+        public List<LatestReportItemViewModel> Reports { get; set; } = new();
+    }
+
+
+}
