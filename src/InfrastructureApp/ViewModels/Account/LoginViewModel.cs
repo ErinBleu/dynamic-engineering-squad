ae13cccdfdc0cb0965ff527652b1e4024cea1872
Phillip Alter PC <phillip.alter@outlook.com>
2026-02-17 10:20:20 -0800

    (fix) rename Username -> UserName

--- src/InfrastructureApp/ViewModels/Account/LoginViewModel.cs
+++ src/InfrastructureApp/ViewModels/Account/LoginViewModel.cs
@@ -5,7 +5,7 @@ namespace InfrastructureApp.ViewModels.Account;
 public class LoginViewModel
 {
     [Required(ErrorMessage = "Username is required")]
-    public string Username { get; set; }
+    public string UserName { get; set; }
     
     [Required(ErrorMessage = "Password is required")]
     [DataType(DataType.Password)]
